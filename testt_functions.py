10e92bba9acdb941ad013e9b3e7360e826a1b076
Sebastian Torres-Olivares <sebastian.torres.o@usach.cl>
2024-08-15 22:05:10 +0300

    I have tested the geometry import things, now it will be included in the main code. I am assuming that the gauss points are ordered in the same way as the elements, if thats not the case all the mapping is wrong.

--- testt_functions.py
+++ testt_functions.py
@@ -1,33 +1,67 @@
 from mpi4py import MPI
 import dolfinx
 from dolfinx import mesh, fem, io
-import gmsh
+import ufl
+import basix
 import numpy as np
 
-
-# Assuming your file is named "your_mesh_file.msh"
+# Read the mesh
 domain, cell_tags, facet_tags = dolfinx.io.gmshio.read_from_msh("neper_files/n2-id1.msh", MPI.COMM_WORLD)
-poly1_entities = cell_tags.find(1)
-poly2_entities = cell_tags.find(2)
+
+dim = domain.topology.dim
+print(f"Mesh topology dimension d={dim}.")
+
+# Create anumpy array containing the orientations per element
+def create_element_to_tag_map(domain, cell_tags):
+    element_to_tag = np.zeros(domain.topology.index_map(domain.topology.dim).size_local, dtype=np.int32)
+    for tag in np.unique(cell_tags.values):
+        cells = cell_tags.find(tag)
+        element_to_tag[cells] = tag
+    return element_to_tag
 
 
-# Create a function space for the cell tags
+# Create tag function on DG0 space
 DG0 = fem.functionspace(domain, ("DG", 0))
-tag_function = fem.Function(DG0, name="Grain")
-
-# Initialize the tag_function with zeros
-tag_function.x.array[:] = 0
-
-# Set the values for the tagged cells
-tag_function.x.array[poly1_entities] = 1
-tag_function.x.array[poly2_entities] = 2
+tag_function = fem.Function(DG0)
+tag_function.x.array[:] = create_element_to_tag_map(domain, cell_tags)
 
 
-# Write mesh to an XDMF file
+# Define quadrature function space
+degree = 2  # or whatever degree you're using
+Qe = basix.ufl.quadrature_element(
+    domain.basix_cell(), value_shape=(), scheme="default", degree=degree
+)
+Q = fem.functionspace(domain, Qe)
+
+# Create a function on the quadrature space
+quad_tags = fem.Function(Q)
+
+# Get the number of quadrature points per cell
+num_cells = domain.topology.index_map(domain.topology.dim).size_local
+dofs_per_cell = Q.dofmap.dof_layout.num_dofs
+
+# Manually assign tag values to quadrature points
+quad_tags_array = np.zeros(num_cells * dofs_per_cell)
+for cell in range(num_cells):
+    cell_tag = tag_function.x.array[cell]
+    start = cell * dofs_per_cell
+    end = (cell + 1) * dofs_per_cell
+    quad_tags_array[start:end] = cell_tag
+
+# Project the cell-wise constant onto the quadrature space
+quad_tags.x.array[:] = quad_tags_array
+
+
+# Write results to XDMF
 out_file = "CP_orientations.xdmf"
 with io.XDMFFile(domain.comm, out_file, "w") as xdmf:
     xdmf.write_mesh(domain)
     xdmf.write_function(tag_function)
+
+print("Quadrature tags shape:", quad_tags.x.array.shape)
+print("Quadrature tags:", quad_tags.x.array)
+
+
 
 
 
